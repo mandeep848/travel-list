5f34b76047b3103f45d896042f7126684bce89c1
MandeepSinh Gohil <gohilmandeep848@gmail.com>
2024-07-14 19:33:44 +0530

    making different files for different components

---
+++ src/components/App.js
@@ -0,0 +1,44 @@
+import { useState } from "react";
+import PackingList from "./PackingList";
+import Stats from "./Stats";
+import Logo from "./Logo";
+import Form from "./Form";
+
+/*const initialItems = [
+  { id: 1, description: "Passports", quantity: 2, packed: false },
+  { id: 2, description: "Socks", quantity: 12, packed: true },
+  { id: 3, description: "charger", quantity: 1, packed: false },
+];*/
+export default function App() {
+  const [items, setItems] = useState([]);
+  function handleAddItems(item) {
+    setItems((items) => [...items, item]);
+  }
+  function handleDeleteItem(id) {
+    setItems((items) => items.filter((item) => item.id !== id));
+  }
+  function handleToggle(id) {
+    setItems((items) =>
+      items.map((item) =>
+        item.id === id ? { ...item, packed: !item.packed } : item
+      )
+    );
+  }
+  function handleclearList() {
+    const confirm = window.confirm("Do you want to delete all the Items");
+    if (confirm) setItems([]);
+  }
+  return (
+    <div className="app">
+      <Logo />
+      <Form onAddItems={handleAddItems} />
+      <PackingList
+        items={items}
+        onDeleteItem={handleDeleteItem}
+        onToggle={handleToggle}
+        clearList={handleclearList}
+      />
+      <Stats items={items} />
+    </div>
+  );
+}
